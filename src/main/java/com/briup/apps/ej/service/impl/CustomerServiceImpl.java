ebc71a2f09ec6020f88d233c5a4c0dd118deb0d3
IFly <1103911246@qq.com>
2019-06-12 13:09:54 +0800

    顾客管理的增删改查的实现 尹双立

--- src/main/java/com/briup/apps/ej/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/briup/apps/ej/service/impl/CustomerServiceImpl.java
@@ -1,11 +1,9 @@
 package com.briup.apps.ej.service.impl;
 
-
 import com.briup.apps.ej.bean.Customer;
 import com.briup.apps.ej.bean.CustomerExample;
 import com.briup.apps.ej.dao.CustomerMapper;
 import com.briup.apps.ej.service.ICustomerService;
-import com.briup.apps.ej.utils.MessageUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -13,7 +11,6 @@ import java.util.List;
 
 @Service
 public class CustomerServiceImpl implements ICustomerService {
-
     @Resource
     private CustomerMapper customerMapper;
 
@@ -24,50 +21,25 @@ public class CustomerServiceImpl implements ICustomerService {
     }
 
     @Override
-    public List<Customer> query(Customer customer) {
-        CustomerExample example = new CustomerExample();
-        CustomerExample.Criteria c =example.createCriteria();
-        if (customer.getPassword()!=null){
-            example.createCriteria().andPasswordLike("%"+customer.getPassword()+"%");
-        }
-        if (customer.getRealname()!=null){
-            example.createCriteria().andRealnameLike("%"+customer.getRealname()+"%");
-        }
-        if (customer.getPhoto()!=null){
-            example.createCriteria().andPhotoLike("%"+customer.getPhoto()+"%");
-        }
-        if (customer.getStatus()!=null){
-            example.createCriteria().andStatusNotLike("%"+customer.getStatus()+"%");
-        }
-        if (customer.getTelephone()!=null){
-            example.createCriteria().andTelephoneLike("%"+customer.getTelephone()+"%");
-        }
-        return customerMapper.selectByExample(example);
-    }
-
-    @Override
-    public Customer findById(long id) {
-        return customerMapper.selectByPrimaryKey(id);
-    }
-
-    @Override
     public void saveOrUpdate(Customer customer) throws Exception {
-        if (customer.getId()==null){
-            customerMapper.insert(customer);
-        }else {
+        if(customer.getId()!=null){
             customerMapper.updateByPrimaryKey(customer);
+        } else {
+            customer.setStatus("正常");
+            customerMapper.insert(customer);
         }
     }
+
 
     @Override
     public void deleteById(long id) throws Exception {
         Customer customer = customerMapper.selectByPrimaryKey(id);
-        if (customer == null) {
-            throw new Exception("不存在此顾客");
-        } else {
-            customerMapper.deleteByPrimaryKey(id);
+        if(customer == null){
+            throw new Exception("要删除的用户信息不存在");
         }
+        customerMapper.deleteByPrimaryKey(id);
     }
+
     @Override
     public void batchDelete(long[] ids) throws Exception {
         for(long id :ids){
